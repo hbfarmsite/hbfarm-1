c3ba09dc8857dbe38bd21142b1af2041868e34ba
hbfarm <129357089+hbfarm@users.noreply.github.com>
2023-04-11 20:50:46 +0000

    atualizacao

--- granja/models.py
+++ granja/models.py
@@ -1,5 +1,83 @@
+''' imports '''
+from datetime import timezone
 from django.db import models
+from django.dispatch import receiver
 
 class Ovo(models.Model):
-    codigo = models.CharField(max_length=10, unique=True)
+    STATUS_CHOICES = (
+        ("armazenado", "Armazenado"),
+        ("chocando", "Chocando"),
+    )
+    data_cadastro = models.DateTimeField(auto_now_add=True, editable=True)
+    codigo = models.CharField(max_length=10, unique=True, editable=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="armazenado", editable=True)
+    chocadeira = models.CharField(max_length=3, default=1)
+    data_chocadeira = models.DateField(null=True, blank=True)
+    dias_chocadeira = models.IntegerField(blank=True, null=True)
+    objects = models.Manager()
 
+    def __str__(self):
+        return f'Ovo { self.codigo } - Status: { self.status }'
+
+    def data_eclosao(self):
+        agora = timezone.now()
+        if self.status == "chocando":
+            estimado = agora + timezone.timedelta(days=21)
+            self.data_chocadeira = timezone.now()
+            return estimado
+        return None
+
+    def tempo_armazenado(self):
+        if self.status == "armazenado":
+            diferenca = timezone.now() - self.data_cadastro
+            return diferenca
+        return None
+
+    def aviso_vencimento(self):
+        if self.status == "armazenado" and self.tempo_armazenado().days >= 28:
+            return True
+        return False
+
+    def set_dias_chocadeira(self):
+        if self.status == "chocando":
+            dias_chocadeira = timezone.now() - self.data_chocadeira
+            self.dias_chocadeira = dias_chocadeira
+            return dias_chocadeira
+        return None
+
+    def get_imagem_ovo(self):
+        if self.dias_chocadeira < 1:
+            return None
+        elif self.dias_chocadeira == 1:
+            return "ovoscopia/1dia.png"
+        elif self.dias_chocadeira > 1 and self.dias_chocadeira <= 21:
+            return f"ovoscopia/{self.dias_chocadeira}dia.png"
+        else:
+            return "pintinho.png"
+
+    def save(self, *args, **kwargs):
+        if not self.codigo:
+            data_atual = timezone.now()
+            ano = int(data_atual.strftime('%y'))
+            mes = int(data_atual.strftime('%m'))
+            dia = int(data_atual.strftime('%d'))
+            ultimo_id_dia = Ovo.objects.filter(
+                data_cadastro__date=data_atual).count()
+            codigo = f'{ano}{mes}{dia}{ultimo_id_dia+1:01d}'
+            self.codigo = codigo
+        super(Ovo, self).save(*args, **kwargs)
+
+class Raca(models.Model):
+    
+
+class Pintinho(models.Model):
+    ovo = models.OneToOneField(Ovo, on_delete=models.CASCADE)
+    codigo = models.CharField(max_length=10, unique=True)
+    raca = models.CharField(max_length=10, blank=True, null=True)
+    nome_cientifico = models.CharField(max_length=50, blank=True, null=True)
+    deficiencia = models.BooleanField(default=False)
+    data_eclosao = models.DateTimeField(default=timezone.now(), blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        self.codigo = "p-" + self.ovo.codigo
+        super(Pintinho, self).save(*args, **kwargs)
