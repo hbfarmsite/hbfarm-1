ca2e4766887dc5cf76f06051691e9e1ea9a6b730
breno <brenoqpl22@gmail.com>
2023-04-17 11:57:57 -0300

    escondendo o settings

--- hbfarm/settings.py
+++ hbfarm/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = "hbfarm.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
